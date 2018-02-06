f8838c3646bc7639be44af56603d8101c4caa150
Caesar <pankobe24@gmail.com>
2018-02-06 09:21:11 +0800

    Refactored project.

--- CIFAR10/pipeline.py
+++ CIFAR10/pipeline.py
@@ -10,6 +10,9 @@
 import numpy as np
 import tensorflow as tf
 from scn_model import SCN_Model
+import utils as utl
+from data import get_data_set
+import pdb
 
 ##################################################################################################################################################################
 #
@@ -20,6 +23,10 @@ from scn_model import SCN_Model
 _MODEL_SAVE_PATH = "./models/"
 _BATCH_SIZE = 50
 _EPOCH_NUM = 250
+_CLASS_NUM = 10
+
+train_keep_prob = [0.9, 0.8, 0.7, 0.6, 0.5]
+test_keep_prob = [1.0, 1.0, 1.0, 1.0, 1.0]
 
 ##################################################################################################################################################################
 #
@@ -58,16 +65,16 @@ keep_prob3 = tf.placeholder(tf.float32)
 keep_prob4 = tf.placeholder(tf.float32)
 keep_prob5 = tf.placeholder(tf.float32)
 keep_prob6 = tf.placeholder(tf.float32)
-keep_prob = {"keep_prob2": keep_prob2, "keep_prob3": keep_prob3, "keep_prob4": keep_prob4, "keep_prob5": keep_prob5, "keep_prob6": keep_prob6}
 
 # Model initialization
-model = SCN_Model(x_image, y_, keep_prob)
+model = SCN_Model(x_image, y_, keep_prob2, keep_prob3, keep_prob4, keep_prob5, keep_prob6)
 
 # Training
 print("Training")
 saver = tf.train.Saver(max_to_keep=0)
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
+test_prediction = np.zeros(shape=(len(test_x), _CLASS_NUM), dtype=np.float32)
 
 for epoch in range(_EPOCH_NUM):
     epoch_num = epoch + 1				# The actual epoch starts from 1
@@ -85,33 +92,48 @@ for epoch in range(_EPOCH_NUM):
         batch_y = train_y[step*_BATCH_SIZE:(step+1)*_BATCH_SIZE-1]
 
         if step % 100 == 0:
-            train_accuracy = sess.run(model.accuracy, feed_dict={image: batch_x, label: batch_y, 
-                                                                 keep_prob: {"keep_prob2": 1.0, "keep_prob3": 1.0, 
-                                                                             "keep_prob4": 1.0, "keep_prob5": 1.0, "keep_prob6": 1.0}})
+            train_accuracy = sess.run(model.accuracy, feed_dict={x: batch_x, y_: batch_y, keep_prob2: 1.0, keep_prob3: 1.0,
+                                                                 keep_prob4: 1.0, keep_prob5: 1.0, keep_prob6: 1.0})
+                                                                 #keep_prob: {"keep_prob2": 1.0, "keep_prob3": 1.0,
+                                                                 #            "keep_prob4": 1.0, "keep_prob5": 1.0, "keep_prob6": 1.0}})
             print("epoch %d, step %d, training accuracy %g" % (epoch_num, step, train_accuracy))
         
-        sess.run(model.optimize, feed_dict={image: batch_x, label: batch_y,
-                                            keep_prob: {"keep_prob2": 0.9, "keep_prob3": 0.8, 
-                                                        "keep_prob4": 0.7, "keep_prob5": 0.6, "keep_prob6": 0.5}})
+        sess.run(model.optimize, feed_dict={x: batch_x, y_: batch_y, keep_prob2: 0.9, keep_prob3: 0.8,
+                                            keep_prob4: 0.7, keep_prob5: 0.6, keep_prob6: 0.5})
 
     if epoch_num >= (_EPOCH_NUM - 10):           # Only save models for the last 10 epochs
         saver.save(sess, save_path=_MODEL_SAVE_PATH, global_step=epoch_num)
         print("Saved checkpoint for epoch %d and training accuracy %g." % (epoch_num, train_accuracy))
-   
-    print("Training finished.")
 
-for i in range(len(test_x)/_BATCH_SIZE):
-    test_batch_x = test_x[i*_BATCH_SIZE:(i+1)*_BATCH_SIZE-1, :]
-    test_batch_y = test_y[i*_BATCH_SIZE:(i+1)*_BATCH_SIZE-1, :]
-    test_prediction[i*_BATCH_SIZE:(i+1)*_BATCH_SIZE-1] = sess.run(model.prediction, feed_dict={image: test_batch_x, label: test_batch_y, 
-                                                            keep_prob: {"keep_prob2": 1.0, "keep_prob3": 1.0,
-                                                                        "keep_prob4": 1.0, "keep_prob5": 1.0, "keep_prob6": 1.0}})
+    for i in range(len(test_x) / _BATCH_SIZE):
+        test_batch_x = test_x[i * _BATCH_SIZE:(i + 1) * _BATCH_SIZE - 1, :]
+        test_batch_y = test_y[i * _BATCH_SIZE:(i + 1) * _BATCH_SIZE - 1, :]
+        test_prediction[i * _BATCH_SIZE:(i + 1) * _BATCH_SIZE - 1, :] = sess.run(model.prediction,
+                                                                              feed_dict={x: test_batch_x, y_: test_batch_y, keep_prob2: 1.0,
+                                                                                         keep_prob3: 1.0, keep_prob4: 1.0, keep_prob5: 1.0, keep_prob6: 1.0})
+
+    # Test accuracy computation
+    test_correct_pred = np.equal(np.argmax(test_prediction, 1), np.argmax(test_y, 1))
+    test_accuracy = test_correct_pred.mean() * 100
+    correct_num = test_correct_pred.sum()
+    test_acc_str = "Epoch {0}. Accuracy on Test Set: {1:.2f}% ({2} / {3}) \n".format(epoch, test_accuracy, correct_num, len(test_x))
+    print(test_acc_str)
+
+print("Training finished.")
+
+for i in range(len(test_x) / _BATCH_SIZE):
+    test_batch_x = test_x[i * _BATCH_SIZE:(i + 1) * _BATCH_SIZE - 1, :]
+    test_batch_y = test_y[i * _BATCH_SIZE:(i + 1) * _BATCH_SIZE - 1, :]
+    test_prediction[i * _BATCH_SIZE:(i + 1) * _BATCH_SIZE - 1, :] = sess.run(model.prediction,
+                                                                             feed_dict={x: test_batch_x, y_: test_batch_y, keep_prob2: 1.0,
+                                                                                        keep_prob3: 1.0, keep_prob4: 1.0, keep_prob5: 1.0, keep_prob6: 1.0})
 
 # Test accuracy computation
-test_correct_pred = tf.equal(tf.argmax(test_prediction, 1), tf.argmax(test_y, 1))
+test_correct_pred = np.equal(np.argmax(test_prediction, 1), np.argmax(test_y, 1))
 test_accuracy = test_correct_pred.mean() * 100
 correct_num = test_correct_pred.sum()
-test_acc_str = "Accuracy on Test Set: {0:.2f}% ({1} / {2}) \n".format(test_accuracy, correct_num, len(test_x))
+test_acc_str = "Epoch {0}. Accuracy on Test Set: {1:.2f}% ({2} / {3}) \n".format(epoch, test_accuracy, correct_num,
+                                                                                 len(test_x))
 print(test_acc_str)
 #lprint("test accuracy %g" % accuracy.eval(session=sess,
 #                                         feed_dict={x: test_x, y_: test_y, keep_prob2: 1.0, keep_prob3: 1.0,
--- CIFAR10/scn_model.py
+++ CIFAR10/scn_model.py
@@ -18,7 +18,7 @@ import functools
 #
 ##################################################################################################################################################################
 
-_WEIGHT_DECAY = 3e-4
+_WEIGHT_DECAY = 1e-3
 
 ##################################################################################################################################################################
 #
@@ -26,18 +26,21 @@ _WEIGHT_DECAY = 3e-4
 #
 ##################################################################################################################################################################
 
-def define_scope(function):
+def define_scope(function, scope=None):
 
     """This is a function decorator that performs lazy instantiation as well as scope definition."""
+
+    attribute = '_cache_' + function.__name__
+    name = scope or function.__name__
 
     @property
     @functools.wraps(function)
     def decorator(self):
         if not hasattr(self, attribute):
-            with tf.variable_scope(function.__name):
+            with tf.variable_scope(name):
                 setattr(self, attribute, function(self))
         return getattr(self, attribute)
-
+    
     return decorator
 
 ##################################################################################################################################################################
@@ -50,24 +53,26 @@ class SCN_Model:
 
     """This class maps the Sparse ConvNet model and encapsulates several core functions."""
 
-    def __init__(self, image, label, keep_prob):
+    def __init__(self, image, label, keep_prob2, keep_prob3, keep_prob4, keep_prob5, keep_prob6):
 
         """
-        :param data: train_x
-        :param target: train_y
-        :param keep_prob: the dictionary which keeps all keeping probabilities
+        :param image: train_x
+        :param label: train_y
+        :param keep_prob:
         """
 
         self.image = image
         self.label = label
+        self.keep_prob2 = keep_prob2
+        self.keep_prob3 = keep_prob3
+        self.keep_prob4 = keep_prob4
+        self.keep_prob5 = keep_prob5
+        self.keep_prob6 = keep_prob6
+
+        # The following 3 lines MUST be placed after those direct assignments above
         self.prediction
         self.optimize
         self.accuracy
-        self.keep_prob2 = keep_prob["keep_prob2"]
-        self.keep_prob3 = keep_prob["keep_prob3"]
-        self.keep_prob4 = keep_prob["keep_prob4"]
-        self.keep_prob5 = keep_prob["keep_prob5"]
-        self.keep_prob6 = keep_prob["keep_prob6"]
 
     @define_scope
     def prediction(self):
@@ -76,90 +81,89 @@ class SCN_Model:
         
         ### Construct the network
         # First layer suit (1 layer suit = 1 conv layer + (dropout) + 1 pooling layer + 1 NiN layer)
-        W_conv1 = weight_variable([3, 3, 3, 300], name="w_conv1")
-        b_conv1 = bias_variable([300], name="b_conv1")
-        h_conv1 = utl._leakyrelu(conv2d(self.image, W_conv1) + b_conv1, alpha=0.33)
-        #h_conv1 = tf.keras.layers.LeakyRelu(conv2d(x_image, W_conv1) + b_conv1, alpha=0.33)
-        h_pool1 = max_pool_2x2(h_conv1)
+        W_conv1 = utl.weight_variable([3, 3, 3, 300], name="w_conv1")
+        b_conv1 = utl.bias_variable([300], name="b_conv1")
+        h_conv1 = utl._leakyrelu(utl.conv2d(self.image, W_conv1) + b_conv1, alpha=0.33)
+        #h_conv1 = tf.keras.layers.LeakyRelu(utl.conv2d(x_image, W_conv1) + b_conv1, alpha=0.33)
+        h_pool1 = utl.max_pool_2x2(h_conv1)
         
-        W_nin1 = weight_variable([1, 1, 300, 300], name="w_nin1")
-        b_nin1 = bias_variable([300], name="b_nin1")
-        h_nin1 = utl._leakyrelu(conv2d(h_pool1, W_nin1) + b_nin1, alpha=0.33)
+        W_nin1 = utl.weight_variable([1, 1, 300, 300], name="w_nin1")
+        b_nin1 = utl.bias_variable([300], name="b_nin1")
+        h_nin1 = utl._leakyrelu(utl.conv2d(h_pool1, W_nin1) + b_nin1, alpha=0.33)
         
         # Second layer suit
-        W_conv2 = weight_variable([2, 2, 300, 600], name="w_conv2")
-        b_conv2 = bias_variable([600], name="b_conv2")
+        W_conv2 = utl.weight_variable([2, 2, 300, 600], name="w_conv2")
+        b_conv2 = utl.bias_variable([600], name="b_conv2")
         
-        h_conv2 = utl._leakyrelu(conv2d(h_nin1, W_conv2) + b_conv2, alpha=0.33)
-        self.keep_prob2 = tf.placeholder(tf.float32)
+        h_conv2 = utl._leakyrelu(utl.conv2d(h_nin1, W_conv2) + b_conv2, alpha=0.33)
+        #self.keep_prob2 = tf.placeholder(tf.float32)
         h_conv2_drop = tf.nn.dropout(h_conv2, self.keep_prob2)
-        h_pool2 = max_pool_2x2(h_conv2_drop)
+        h_pool2 = utl.max_pool_2x2(h_conv2_drop)
         
-        W_nin2 = weight_variable([1, 1, 600, 600], name="w_nin2")
-        b_nin2 = bias_variable([600], name="b_nin2")
-        h_nin2 = utl._leakyrelu(conv2d(h_pool2, W_nin2) + b_nin2, alpha=0.33)
+        W_nin2 = utl.weight_variable([1, 1, 600, 600], name="w_nin2")
+        b_nin2 = utl.bias_variable([600], name="b_nin2")
+        h_nin2 = utl._leakyrelu(utl.conv2d(h_pool2, W_nin2) + b_nin2, alpha=0.33)
         
         # Third layer suit
-        W_conv3 = weight_variable([2, 2, 600, 900], name="w_conv3")
-        b_conv3 = bias_variable([900], name="b_conv3")
+        W_conv3 = utl.weight_variable([2, 2, 600, 900], name="w_conv3")
+        b_conv3 = utl.bias_variable([900], name="b_conv3")
         
-        h_conv3 = utl._leakyrelu(conv2d(h_nin2, W_conv3) + b_conv3, alpha=0.33)
-        self.keep_prob3 = tf.placeholder(tf.float32)
+        h_conv3 = utl._leakyrelu(utl.conv2d(h_nin2, W_conv3) + b_conv3, alpha=0.33)
+        #self.keep_prob3 = tf.placeholder(tf.float32)
         h_conv3_drop = tf.nn.dropout(h_conv3, self.keep_prob3)
-        h_pool3 = max_pool_2x2(h_conv3_drop)
+        h_pool3 = utl.max_pool_2x2(h_conv3_drop)
         
-        W_nin3 = weight_variable([1, 1, 900, 900], name="w_nin3")
-        b_nin3 = bias_variable([900], name="b_nin3")
-        h_nin3 = utl._leakyrelu(conv2d(h_pool3, W_nin3) + b_nin3, alpha=0.33)
+        W_nin3 = utl.weight_variable([1, 1, 900, 900], name="w_nin3")
+        b_nin3 = utl.bias_variable([900], name="b_nin3")
+        h_nin3 = utl._leakyrelu(utl.conv2d(h_pool3, W_nin3) + b_nin3, alpha=0.33)
         
         # Fourth layer suit
-        W_conv4 = weight_variable([2, 2, 900, 1200], name="w_conv4")
-        b_conv4 = bias_variable([1200], name="b_conv4")
+        W_conv4 = utl.weight_variable([2, 2, 900, 1200], name="w_conv4")
+        b_conv4 = utl.bias_variable([1200], name="b_conv4")
         
-        h_conv4 = utl._leakyrelu(conv2d(h_nin3, W_conv4) + b_conv4, alpha=0.33)
-        self.keep_prob4 = tf.placeholder(tf.float32)
+        h_conv4 = utl._leakyrelu(utl.conv2d(h_nin3, W_conv4) + b_conv4, alpha=0.33)
+        #self.keep_prob4 = tf.placeholder(tf.float32)
         h_conv4_drop = tf.nn.dropout(h_conv4, self.keep_prob4)
-        h_pool4 = max_pool_2x2(h_conv4_drop)
+        h_pool4 = utl.max_pool_2x2(h_conv4_drop)
         
-        W_nin4 = weight_variable([1, 1, 1200, 1200], name="w_nin4")
-        b_nin4 = bias_variable([1200], name="b_nin4")
-        h_nin4 = utl._leakyrelu(conv2d(h_pool4, W_nin4) + b_nin4, alpha=0.33)
+        W_nin4 = utl.weight_variable([1, 1, 1200, 1200], name="w_nin4")
+        b_nin4 = utl.bias_variable([1200], name="b_nin4")
+        h_nin4 = utl._leakyrelu(utl.conv2d(h_pool4, W_nin4) + b_nin4, alpha=0.33)
         
         # Fifth layer suit
-        W_conv5 = weight_variable([2, 2, 1200, 1500], name="w_conv5")
-        b_conv5 = bias_variable([1500], name="b_conv5")
+        W_conv5 = utl.weight_variable([2, 2, 1200, 1500], name="w_conv5")
+        b_conv5 = utl.bias_variable([1500], name="b_conv5")
         
-        h_conv5 = utl._leakyrelu(conv2d(h_nin4, W_conv5) + b_conv5, alpha=0.33)
-        self.keep_prob5 = tf.placeholder(tf.float32)
+        h_conv5 = utl._leakyrelu(utl.conv2d(h_nin4, W_conv5) + b_conv5, alpha=0.33)
+        #self.keep_prob5 = tf.placeholder(tf.float32)
         h_conv5_drop = tf.nn.dropout(h_conv5, self.keep_prob5)
-        h_pool5 = max_pool_2x2(h_conv5_drop)
+        h_pool5 = utl.max_pool_2x2(h_conv5_drop)
         
-        W_nin5 = weight_variable([1, 1, 1500, 1500], name="w_nin5")
-        b_nin5 = bias_variable([1500], name="b_nin5")
-        h_nin5 = utl._leakyrelu(conv2d(h_pool5, W_nin5) + b_nin5, alpha=0.33)
+        W_nin5 = utl.weight_variable([1, 1, 1500, 1500], name="w_nin5")
+        b_nin5 = utl.bias_variable([1500], name="b_nin5")
+        h_nin5 = utl._leakyrelu(utl.conv2d(h_pool5, W_nin5) + b_nin5, alpha=0.33)
         
         # Sixth conv layer
-        W_conv6 = weight_variable([2, 2, 1500, 1800], name="w_conv6")
-        b_conv6 = bias_variable([1800], name="b_conv6")
+        W_conv6 = utl.weight_variable([2, 2, 1500, 1800], name="w_conv6")
+        b_conv6 = utl.bias_variable([1800], name="b_conv6")
         
-        h_conv6 = utl._leakyrelu(conv2d(h_nin5, W_conv6) + b_conv6, alpha=0.33)
-        self.keep_prob6 = tf.placeholder(tf.float32)
+        h_conv6 = utl._leakyrelu(utl.conv2d(h_nin5, W_conv6) + b_conv6, alpha=0.33)
+        #self.keep_prob6 = tf.placeholder(tf.float32)
         h_conv6_drop = tf.nn.dropout(h_conv6, self.keep_prob6)
         
-        W_nin6 = weight_variable([1, 1, 1800, 1800], name="w_nin6")
-        b_nin6 = bias_variable([1800], name="b_nin6")
-        h_nin6 = utl._leakyrelu(conv2d(h_conv6_drop, W_nin6) + b_nin6, alpha=0.33)
+        W_nin6 = utl.weight_variable([1, 1, 1800, 1800], name="w_nin6")
+        b_nin6 = utl.bias_variable([1800], name="b_nin6")
+        h_nin6 = utl._leakyrelu(utl.conv2d(h_conv6_drop, W_nin6) + b_nin6, alpha=0.33)
         
         h_nin6_flat = tf.reshape(h_nin6, [-1, 1 * 1 * 1800])
         
         # Softmax classification layer
-        W_sm = weight_variable([1 * 1 * 1800, 10], name="w_sm")
-        b_sm = bias_variable([10], name="b_sm")
+        W_sm = utl.weight_variable([1 * 1 * 1800, 10], name="w_sm")
+        b_sm = utl.bias_variable([10], name="b_sm")
         
         y_conv = tf.matmul(h_nin6_flat, W_sm) + b_sm
         
         return y_conv
-
 
     @define_scope
     def optimize(self):
